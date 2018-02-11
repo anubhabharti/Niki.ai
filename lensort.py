ddaaf3f6211185d27b14a7a233c843f3649d4bbc
anubhabharti <cse1442@cemk.ac.in>
2018-02-12 01:52:21 +0530

    Lint fixed and updated doc-string

--- lensort.py
+++ lensort.py
@@ -6,13 +6,18 @@ Author : Anubha Bharti
 
 
 def sorting_list(in_list):
+    """Sort space delimted string
+
+    :param in_list: list, space delimited string
+    :return: list, sorted list
+    """
     try:
         return sorted(in_list, key=len)
     except Exception as e:
         print " Exception Encountered !! Error : {}".format(e)
 
 
-if __name__ ==  "__main__":
+if __name__ == "__main__":
     try:
         input_list = [x for x in raw_input("Please enter a string separated by spaces: ").split()]
         print sorting_list(input_list)
